0f818eac2476cc3f48222750a8a27a12d11ea6a7
Sahil <fromsahil@gmail.com>
2015-10-05 20:47:26 -0400

    location changed

--- Sahil/script.js
+++ Sahil/script.js
@@ -1,10 +1,16 @@
-
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -23.345, lng: 130.144},
-    zoom: 8
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 8});
+  var geocoder = new google.maps.Geocoder;
+  geocoder.geocode({'address': 'Detroit'}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      window.alert('Geocode was not successful for the following reason: ' +
+          status);
+    }
   });
 }
-
-    
