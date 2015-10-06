477579e37e61b8ccb11e5aaa561025c6aeef41b1
Amanda <a.vacharat@gmail.com>
2015-10-06 11:56:03 -0400

    randomizes locations, need to get random locations to waypoint

--- Amanda/app.js
+++ Amanda/app.js
@@ -10,11 +10,35 @@ function customlocation(name, x, y, info) {
 
 var grandCircus = new customlocation ("Grand Circus", 42.335879, -83.049745, "A programming bootcamp");
 var joseTacos = new customlocation ("Jose's Tacos", 42.335608, -83.046593, "Great place for tacos");
-var pureDetroitGM = new customlocation ("Pure Detroit GM Renaissance Center", 42.36, -83.039795, "One of 3 Detroit's significant landmarks");
-var pureDetroitFisher = new customlocation ("Pure Detroit Fisher Building", 42.369803, -83.077457, "Another one of Pure Detroit's significant landmarks");
-var pureDetroit = new customlocation ("Pure Detroit", 42.329545, -83.04614, "Pure Detroit's Guardian Building location");
+var detroitBikes = new customlocation ("Detroit Bikes", 42.33313, -83.048943, "Bicycles manufactured right here in Detroit!");
+var easternMarket = new customlocation ("Eastern Market", 42.34556, -83.04306, "Fresh Food!");
+var johnVarvatos = new customlocation ("John Varvatos", 42.335133, -83.049261, "Take a gander at Detroit's born an raised fashion designer John Varvatos clothing and accessories");
+var nojoKicks = new customlocation ("Nojo Kicks", 42.3341983, -83.0463134, "Detroit's famous sneaker boutique");
+var campusMartius = new customlocation ("Campus Martius", 42.3317249, -83.0465006, "Get food and beverages while being entertained by live music and other festivities");
+var comericaPark = new customlocation ("Comerica Park", 42.3389984, -83.0485197, "Catch a Tigers game or visit where they play!");
+var fordField = new customlocation ("Ford Field", 42.3389984, -83.0485197, "Catch a Lions game or visit where they play!");
+var detroitOperaHouse = new customlocation ("Detroit Opera House", 42.3389984, -83.0485197, "Check out the opulent performing arts venue!");
+var foxTheater = new customlocation ("Fox Theater", 42.3383254, -83.0526774, "Check out the historical Fox Theater, where many famous performances have occurred");
 
-var allLocationsArray = [grandCircus, joseTacos];
+var allLocationsArray = [grandCircus, joseTacos, detroitBikes, easternMarket, johnVarvatos, nojoKicks,
+campusMartius, comericaPark, fordField, detroitOperaHouse, foxTheater];
+
+var selectedLocationsArray= [];
+
+function randomizeLocation() {
+	var randomNumber1 = Math.floor(Math.random()*allLocationsArray.length);
+	for (i = 0; i<randomNumber1; i++) {
+		var randomNumber2 = Math.floor(Math.random()*allLocationsArray.length);
+		console.log(allLocationsArray[i]);
+		selectedLocationsArray.push(allLocationsArray[i]);
+	}
+}
+
+function returnLocations(){
+	
+}
+
+randomizeLocation();
 
 
 function initMap() {
@@ -37,9 +61,9 @@ function calcRoute(directionsService, directionsDisplay) {
 	directionsService.route({
 		origin: grandCircusmap,
 		destination: grandCircusmap,
-		waypoints: [{location: new google.maps.LatLng(pureDetroitGM.coordinates[0], pureDetroitGM.coordinates[1])},
+		waypoints: [{location: new google.maps.LatLng(detroitBikes.coordinates[0], detroitBikes.coordinates[1])},
 		{location: new google.maps.LatLng(joseTacos.coordinates[0], joseTacos.coordinates[1])},
-		{location: new google.maps.LatLng(pureDetroitFisher.coordinates[0], pureDetroitFisher.coordinates[1])}],
+		{location: new google.maps.LatLng(detroitOperaHouse.coordinates[0], detroitOperaHouse.coordinates[1])}],
 		optimizeWaypoints: true,
 		travelMode: google.maps.TravelMode.WALKING
 	}, function(response, status) {
