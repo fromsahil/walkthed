33b74ce5115f086b77b3f528ff26fcacee775f37
Amanda <a.vacharat@gmail.com>
2015-10-08 16:23:50 -0400

    checks if within box but huge delay

--- Master/script/onclick.js
+++ Master/script/onclick.js
@@ -25,31 +25,40 @@ function getLocation() {
 
 
 
- // function checkLocation(position) {
- //    var latitude = position.coords.latitude;
- //    var longitude = position.coords.longitude;
- //    if (42.325<latitude>42.345 && -83.0<longitude>-83.067) {
- //    	var address = ***reverse geocode converted lat lng to text
- //    	document.getElementById("startingLocation").value = address;
- //    }
- //    else {
- //    	alert("Your starting address is not within our current limits.");
- //    }
+ function checkLocation(position) {
+ 	navigator.geolocation.getCurrentPosition(function(pos) {
+		geocoder = new google.maps.Geocoder();
+		var address = document.getElementById("startingLocation").value;
+		console.log(address);
+		geocoder.geocode({'address': address}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				console.log(results);
+				var userLat= results[0].geometry.location.J;
+				var userLng= results[0].geometry.location.M;
+				if ((userLat<42.3225 || userLat>42.345) || (userLng<-83.067 || userLng>-83.0)) {
+					alert("Your starting address is not within our current limits.");
+				}
+        		
 
- //    alert("Latitude : " + latitude + " Longitude: " + longitude);
- // }
+        	}
+		});
+	});
+    // if (42.325<latitude>42.345 && -83.0<longitude>-83.067) {
+    // 	var address = ***reverse geocode converted lat lng to text
+    // 	document.getElementById("startingLocation").value = address;
+    // }
+    // else {
+    // 	alert("Your starting address is not within our current limits.");
+    // }
+
+    // alert("Latitude : " + latitude + " Longitude: " + longitude);
+ }
 
 
-// function showPosition(position) {
-// 	var geocoder = new google.maps.Geocoder();
-// 	var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-// 	// console.log(geocoder.geocode({'location': latlng}));
-//     //document.getElementById("startingLocation").value = "test + " + geocoder.geocode({'location': latlng});
-// }
 
 
-
+var submit = document.getElementById("submitForm");
+submit.addEventListener("click", checkLocation, false);
 
 var getLoc = document.getElementById("currentLocation");
 getLoc.addEventListener("click", getLocation, false);
