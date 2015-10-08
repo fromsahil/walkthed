3935c2a34a608c159a80d8e74bf0cb8eb33b0c8e
Amanda <a.vacharat@gmail.com>
2015-10-08 14:18:45 -0400

    separate pages, working on geocoding

--- Master/script/map.js
+++ Master/script/map.js
@@ -19,32 +19,58 @@ var waypts=[];
 
 
 //create object
-function customlocation(name, x, y, info, time) {
+function customlocation(name, x, y, info, time, type) {
 	this.name = name;
 	this.coordinates = [x, y];
 	this.someInfo = info;
 	this.time = time;
+	this.type = type;
 
 }
 
 
 //create a few objects using the above constructor
 
-var grandCircus = new customlocation ("Grand Circus", 42.335879, -83.049745, "A programming bootcamp", 300);
-var joseTacos = new customlocation ("Jose's Tacos", 42.335608, -83.046593, "Great place for tacos", 500);
-var detroitBikes = new customlocation ("Detroit Bikes", 42.33313, -83.048943, "Bicycles manufactured right here in Detroit!", 300);
-var easternMarket = new customlocation ("Eastern Market", 42.34556, -83.04306, "Fresh Food!", 500);
-var johnVarvatos = new customlocation ("John Varvatos", 42.335133, -83.049261, "Take a gander at Detroit's born an raised fashion designer John Varvatos clothing and accessories", 300);
-var nojoKicks = new customlocation ("Nojo Kicks", 42.3341983, -83.0463134, "Detroit's famous sneaker boutique", 300);
-var campusMartius = new customlocation ("Campus Martius", 42.3317249, -83.0465006, "Get food and beverages while being entertained by live music and other festivities", 600);
-var comericaPark = new customlocation ("Comerica Park", 42.3389984, -83.0485197, "Catch a Tigers game or visit where they play!", 400);
-var fordField = new customlocation ("Ford Field", 42.3389984, -83.0485197, "Catch a Lions game or visit where they play!", 400);
-var detroitOperaHouse = new customlocation ("Detroit Opera House", 42.3389984, -83.0485197, "Check out the opulent performing arts venue!", 500);
-var foxTheater = new customlocation ("Fox Theater", 42.3383254, -83.0526774, "Check out the historical Fox Theater, where many famous performances have occurred", 500);
+var grandCircus = new customlocation ("Grand Circus", 42.335879, -83.049745, "A programming bootcamp", 300, "Landmark");
+var joseTacos = new customlocation ("Jose's Tacos", 42.335608, -83.046593, "Great place for tacos", 500, "Restaurant");
+var detroitBikes = new customlocation ("Detroit Bikes", 42.33313, -83.048943, "Bicycles manufactured right here in Detroit!", 300, "Shopping");
+var easternMarket = new customlocation ("Eastern Market", 42.34556, -83.04306, "Fresh Food!", 500, "Landmark");
+var johnVarvatos = new customlocation ("John Varvatos", 42.335133, -83.049261, "Take a gander at Detroit's born an raised fashion designer John Varvatos clothing and accessories", 300, "Shopping");
+var nojoKicks = new customlocation ("Nojo Kicks", 42.3341983, -83.0463134, "Detroit's famous sneaker boutique", 300, "Shopping");
+var campusMartius = new customlocation ("Campus Martius", 42.3317249, -83.0465006, "Get food and beverages while being entertained by live music and other festivities", 600, "Landmark");
+var comericaPark = new customlocation ("Comerica Park", 42.3389984, -83.0485197, "Catch a Tigers game or visit where they play!", 400, "Landmark");
+var fordField = new customlocation ("Ford Field", 42.3389984, -83.0485197, "Catch a Lions game or visit where they play!", 400, "Landmark");
+var detroitOperaHouse = new customlocation ("Detroit Opera House", 42.3389984, -83.0485197, "Check out the opulent performing arts venue!", 500, "Landmark");
+var foxTheater = new customlocation ("Fox Theater", 42.3383254, -83.0526774, "Check out the historical Fox Theater, where many famous performances have occurred", 500, "Landmark");
+var sweetWaterTavern = new customlocation ("Sweet Water Tavern", 42.330522, -83.046867, "A great wing destination!", 1800, "Restaurant");
+var harborHouse = new customlocation ("Harbor House", 42.330522, -83.046867, "Historic Sea Food Restaurant in the city!", 1800, "Restaurant");
+var sportsMania = new customlocation ("Sports Mania", 42.330522, -83.046867, "Famous Detroit Sports Shop", 600, "Shopping");
+var towerBarber = new customlocation ("Tower Barber", 42.330522, -83.046867, "Get a haircut at Detroit's famous barber shop!", 1200, "Shopping");
+var detroitTattoo = new customlocation ("Detroit Tattoo Shop", 42.330522, -83.046867, "Pay a visit to Detroit's famous tattoo parlor ", 600, "Shopping");
+var gardellaFurniture = new customlocation ("Gardella Furniture", 42.3519252, -83.0320371, "Opened in 1939 by Louis Gardella Sr. This store targets mid to high furniture buyers with antiquated taste", 600, "Shopping");
+var spectacles = new customlocation ("Spectacles", 42.3358422, -83.0464188, "Trendy unisex clothing store", 600, "Shopping");
+var moosejaw = new customlocation ("Moosejaw", 42.3338298, -83.0489466, "More trendy unisex clothing!", 600, "Shopping");
+var detroitVsEverybody = new customlocation ("Detroit Vs Everybody", 42.334167, -83.0432783, "Detroit's famous clothing and accessories store", 1200, "Shopping");
+var detroitWaterIce = new customlocation ("Detroit Water Ice", 42.3324303, -83.0473459, "Mitch Albom's new Detroit Dessert place", 600, "Shopping");
+var slices = new customlocation ("Slices", 42.3325434, -83.0477701, "Get amazing pizza at a great price!", 600, "Shopping");
+var capitolPark = new customlocation ("Capitol Park", 42.3329106, -83.0494783, "Come see Detroit's famous landmark of liberty!", 600, "Landmark");
+var spiritOfDetroit = new customlocation ("Spirit of Detroit", 42.3293023, -83.0445377, "Pay a visit to the Spirit of Detroit statue, built in 1958, which represents the city of Detroit", 600, "Landmark");
+var joeLouisMonument = new customlocation ("Joe Louis Monument", 42.3286701, -83.0445116, "This represents one of the greatest boxers of all time, also known as 'The Fist' ", 600, "Landmark");
+var hartPlaza = new customlocation ("Hart Plaza", 42.327307, -83.0442621, "The area where Hart Plaza stands today is believed to be where Antoine Laumet de La Mothe, sieur de Cadillac landed in 1701", 600, "Landmark");
+var vicentes = new customlocation ("Vicente's Cuban Cuisine", 42.330522, -83.046867, "A great place to indulge in a Cuban lunch ", 1800, "Restaurant");
+var texasDeBrazil = new customlocation ("Texas De Brazil", 42.330522, -83.046867, "All you can eat steak house!", 1800, "Restaurant");
+var lafayetteConey = new customlocation ("Lafayette Coney Island", 42.330522, -83.046867, "Famous family owned and operated, since 1917", 1800, "Restaurant");
+var roast = new customlocation ("Roast", 42.330522, -83.046867, "Upscale Steakhouse", 1800, "Restaurant");
+var bucharest = new customlocation ("Bucharest Grill", 42.330522, -83.046867, "Get your middle eastern bites here!", 1800, "Restaurant");
+var fishbones = new customlocation ("Fishbones", 42.330522, -83.046867, "Cajun Dishes", 1800, "Restaurant");
+
 
 //an array of all of the location objects
 var allLocationsArray = [joseTacos, detroitBikes, easternMarket, johnVarvatos, nojoKicks,
-campusMartius, comericaPark, fordField, detroitOperaHouse, foxTheater];
+campusMartius, comericaPark, fordField, detroitOperaHouse, foxTheater, sweetWaterTavern,
+harborHouse, sportsMania, towerBarber, detroitTattoo, gardellaFurniture, spectacles, moosejaw,
+detroitVsEverybody, detroitWaterIce, slices, capitolPark, spiritOfDetroit, joeLouisMonument,
+hartPlaza, vicentes, texasDeBrazil, lafayetteConey, roast, bucharest, fishbones];
 
 var justRestaurants= [];
 
@@ -193,7 +219,10 @@ function checkRouteisRightLength(time) {
 	return rightTime
 }
 
+
 google.maps.event.addDomListener(window, 'load', initMap);
+
+
 
 
 		
--- Master/script/onclick.js
+++ Master/script/onclick.js
@@ -8,3 +8,23 @@ function myFunction() {
     console.log("do something");
    
 }
+
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    document.getElementById("startingLocation").value = "test + " + position.coords.latitude;
+}
+
+
+
+
+var getLoc = document.getElementById("currentLocation");
+getLoc.addEventListener("click", getLocation, false);
