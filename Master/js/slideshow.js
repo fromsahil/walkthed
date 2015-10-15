fbbc550ba7b720b199a5c15a3e1777a66dbc622f
Sahil <fromsahil@gmail.com>
2015-10-15 11:43:48 -0400

    link to page fixed

--- Master/js/slideshow.js
+++ Master/js/slideshow.js
@@ -5,34 +5,33 @@
 $(document).ready(function(){
 	$.backstretch(
 		[
-	          
-	          "img/The_Sweep.jpg",
-	          "img/chrysler-house-exterior-452x452.jpg",
-	          "img/tumblr_meb2l8WLyx1rkc1w2o1_1280.jpg",
-	          "img/Best-Spring-Activities-in-Detroit-Peruse-Farmers-Markets.jpg",
-	          "img/joelouis.jpg"
-	        ], 
-	        {
-	            fade: 2500,
-	            duration: 2000
-	        });
+
+		"img/The_Sweep.jpg",
+		"img/chrysler-house-exterior-452x452.jpg",
+		"img/tumblr_meb2l8WLyx1rkc1w2o1_1280.jpg",
+		"img/Best-Spring-Activities-in-Detroit-Peruse-Farmers-Markets.jpg",
+		"img/joelouis.jpg"
+		], 
+		{
+			fade: 2500,
+			duration: 2000
+		});
 	
 
 	$('.backstretch').addClass("dim");
+	$('#homepage').show();
+	$('#mapInfo').hide(); 
+	$('#startDtour').click(function(){
+		$ ('#homepage').fadeOut('slow'); 
+		$ ('#mapInfo').fadeIn('slow'); 
+		return false;
+	}); 
 });
 
-  $(document).ready(function() {
-       $('#startInfo').show();
-       $('#mapInfo').hide(); 
-       $('#startDtour').click(function(){ 
-       $ ('#startInfo').hide(); 
-       $ ('#mapInfo').show("fade", 10); 
-      return false;
-           }); 
-  });
 
 
 
 
 
- 
+
+
